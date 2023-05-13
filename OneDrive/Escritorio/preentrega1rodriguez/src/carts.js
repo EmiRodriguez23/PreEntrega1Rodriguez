dd9ff23129410120a6cf02a48dd42aa5c39deb52
EmiRodriguez23 <emilianojrodriguez23@gmail.com>
2023-05-13 12:57:41 -0300

    first commit

---
+++ OneDrive/Escritorio/preentrega1rodriguez/src/carts.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const router = express.Router();
+const cartsFilePath = './cartsgi.json';
+
+// Crear un nuevo carrito
+router.post('/', (req, res) => {
+  const carts = JSON.parse(fs.readFileSync(cartsFilePath));
+  const newCart = {
+    id: uuidv4(),
+    products: []
+  };
+  carts.push(newCart);
+  fs.writeFileSync(cartsFilePath, JSON.stringify(carts, null, 2));
+  res.send(newCart);
+});
+
+// Listar los productos de un carrito por su id
+router.get('/:cid', (req, res) => {
+  const carts = JSON.parse(fs.readFileSync(cartsFilePath));
+  const cart = carts.find(c => c.id == req.params.cid);
+  if (cart) {
+    res.send(cart.products);
+  } else {
+    res.status(404).send({ error: 'Carrito no encontrado' });
+  }
+});
+
+// Agregar un producto a un carrito
+router.post('/:cid/product/:pid', (req, res) => {
+  const carts = JSON.parse(fs.readFileSync(cartsFilePath));
+  const cartIndex = carts.findIndex(c => c.id == req.params.cid);
+  if (cartIndex != -1) {
+    const cart = carts[cartIndex];
+    const existingProduct = cart.products.find(p => p.id == req.params.pid);
+    if (existingProduct) {
+      existingProduct.quantity++;
+    } else {
+      cart.products.push({
+        product: req.params.pid,
+        quantity: 1
+      });
+    }
+    fs.writeFileSync(cartsFilePath, JSON.stringify(carts, null, 2));
+    res.send(cart.products);
+  } else {
+    res.status(404).send({ error: 'Carrito no encontrado' });
+  }
+});
+
+module.exports = router;
